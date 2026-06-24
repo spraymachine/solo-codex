cdcd1d0c253387f4ac7d5639296eabca7020425b
Mani Dodla <manik5621@gmail.com>
2026-06-24 12:34:09 +0530

    f

--- src/lib/books/search.ts
+++ src/lib/books/search.ts
@@ -11,11 +11,9 @@ export async function searchGoogleBooks(query: string, userId?: string): Promise
     }
   }
 
-  const key = process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY;
   const url = new URL("https://www.googleapis.com/books/v1/volumes");
   url.searchParams.set("q", query);
   url.searchParams.set("maxResults", "10");
-  if (key) url.searchParams.set("key", key);
 
   const res = await fetch(url.toString());
   if (!res.ok) return [];
